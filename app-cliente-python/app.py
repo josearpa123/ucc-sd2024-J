e5cc915abfa90ba9503c179d19373c74cd71c786
jose-arias <joseariaspantoj@gmail.com>
2024-10-12 22:27:30 -0500

    Cliente aplicacion python 🚧

---
+++ app-cliente-python/app.py
@@ -0,0 +1,25 @@
+import psycopg2
+import time
+import random
+
+DB_URL = "dbname='postgres' user='postgres' host='localhost' password='postgres'"
+
+def main():
+    conn = psycopg2.connect(DB_URL)
+    cursor = conn.cursor()
+    try:
+        while True:
+            wait_time = random.randint(1, 10)  # Espera aleatoria entre 1 y 10 segundos
+            time.sleep(wait_time)
+            cursor.execute("SELECT id, name FROM dummy_table")
+            results = cursor.fetchall()
+            for row in results:
+                print(f'Python Resultado: is: {row[0]}, name: {row[1]}')  # Muestra tanto el id como el nombre
+    except Exception as e:
+        print(f'Error: {e}')
+    finally:
+        cursor.close()
+        conn.close()
+
+if __name__ == "__main__":
+    main()
